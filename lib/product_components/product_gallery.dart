4a83b488a222094c1924aacb6018a7b4385be825
Orion028 <sumansasmal028@gmail.com>
2021-06-22 23:49:10 +0530

    gallery

--- lib/product_components/product_gallery.dart
+++ lib/product_components/product_gallery.dart
@@ -4,8 +4,185 @@ import '../const.dart';
 class ProductGallery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [],
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 80,
+                  width: 200,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    'Food product'.toUpperCase(),
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: PrimaryColor,
+                      fontFamily: 'Cera',
+                      fontWeight: FontWeight.w700,
+                      decoration: TextDecoration.none,
+                      height: 0.7,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  width: 200,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    'Food product'.toUpperCase(),
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: PrimaryColor,
+                      fontFamily: 'Cera',
+                      fontWeight: FontWeight.w700,
+                      decoration: TextDecoration.none,
+                      height: 0.7,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  width: 200,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    'Food product'.toUpperCase(),
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: PrimaryColor,
+                      fontFamily: 'Cera',
+                      fontWeight: FontWeight.w700,
+                      decoration: TextDecoration.none,
+                      height: 0.7,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 80,
+                  width: 200,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    'Food product'.toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: PrimaryColor,
+                      fontFamily: 'Cera',
+                      fontWeight: FontWeight.w700,
+                      decoration: TextDecoration.none,
+                      height: 0.7,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  width: 200,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    'Food product'.toUpperCase(),
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: PrimaryColor,
+                      fontFamily: 'Cera',
+                      fontWeight: FontWeight.w700,
+                      decoration: TextDecoration.none,
+                      height: 0.7,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  width: 200,
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    'Food product'.toUpperCase(),
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: PrimaryColor,
+                      fontFamily: 'Cera',
+                      fontWeight: FontWeight.w700,
+                      decoration: TextDecoration.none,
+                      height: 0.7,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
