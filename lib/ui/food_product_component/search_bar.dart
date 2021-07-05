09061d94f9d954b2d704500a067db48ae5a731b0
Parthvsquare <parthvsquare@gmail.com>
2021-07-05 18:26:38 +0530

    refractored and added new product page

---
+++ lib/ui/food_product_component/search_bar.dart
@@ -0,0 +1,13 @@
+class SearchBar extends StatefulWidget {
+  const SearchBar({Key? key}) : super(key: key);
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
